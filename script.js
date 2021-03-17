b3495f8bbecea7564f5044f11a5a177301d15e02
IainLR <iainrobertzon@gmail.com>
2021-03-16 22:30:59 -0500

    now the JS can loop through multiple messages

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = 'My name is Iain'
+const textArr = ['My name is Iain', 'Welcome', 'This is Fine']
+// const text = 'My name is Iain'
+let textChoice = 0
+// let text = textArr[textChoice]
 let idx = 1
 let speed = 300 / speedEl.value
 
 writeText()
 
 function writeText() {
+  let text = textArr[textChoice]
   textEl.innerText = text.slice(0, idx)
-
   idx++
 
   if (idx > text.length) {
+    console.log(textChoice)
+
     idx = 1
+    if (textChoice >= textArr.length - 1) {
+      textChoice = 0
+    }
+    textChoice++
   }
 
   setTimeout(writeText, speed)
